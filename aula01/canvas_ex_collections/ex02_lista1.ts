f9db070e98c9ebf2566fd88928249a87e1ddedc0
mwendell <mwendellsmce@gmail.com>
2026-01-30 19:59:36 -0300

    adicionando ex02 da lsita 1 de typescript

---
+++ aula01/canvas_ex_collections/ex02_lista1.ts
@@ -0,0 +1,12 @@
+import leia = require('readline-sync')
+
+let numeros = Array<number>()
+numeros = [2, 5, 1, 3, 4, 9, 7, 8, 10, 6]
+
+let user = leia.questionInt('Digite o numero que voce deseja encontrar: ')
+
+if(numeros.includes(user)){
+console.log(`O número ${user} está localizado na posição: ${numeros.indexOf(user)}`)
+}else{
+    console.log(`O número ${user} não foi encontrado!`)
+}
